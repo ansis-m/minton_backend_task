4ea49805610d90d07066e37f6ce072a2a21df585
ansis@home <ansis@home>
2024-01-13 16:17:11 +0200

    init

---
+++ src/main/java/com/example/mintos/backend/MintosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mintos.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintosBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MintosBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mintos/backend/MintosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mintos.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MintosBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
